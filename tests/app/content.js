14da3bcfbcb96053c27685258618caf53bb28819
Dwayne Ford <dwayneford@gmail.com>
2015-05-09 12:42:48 -0400

    Contents

--- tests/app/content.js
+++ tests/app/content.js
@@ -1,8 +1,11 @@
 describe('Framework CONTENTS (methods)', function() {
   var path = require('path'),
-      appContents = require(path.join(appSrc, 'app', 'contents'));
+      appContents = require(path.join(appSrc, 'app', 'content'));
 
-  it('should have a Router', function() {
+  it('should have a Router method', function() {
     expect(appContents.hasOwnProperty('router')).to.be.true;
+  });
+  it('should have a View method', function() {
+    expect(appContents.hasOwnProperty('view')).to.be.true;
   });
 });
