b30864ef316c3400f1b4cb8b3506055a7542eb3b
Dwayne Ford <dwayneford@gmail.com>
2015-05-10 04:36:49 -0400

    Refactor of test task

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,23 @@ var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
     chai = require('chai'),
     expect = chai.expect,
-    sinon = require('sinon');
-
-    global.expect = expect;
-    global.sinon = sinon;
-    global.appSrc = path.join(process.env.INIT_CWD, 'src');
+    sinon = require('sinon'),
+    tap = require('gulp-tap');
 
 gulp.task('test', function() {
+  global.expect = expect;
+  global.path = path;
+  global.sinon = sinon;
+  global.sourceFiles = global.sourceFiles || {};
+
   gulp.src('tests/**/*.js')
+    .pipe(tap(function(file) {
+      var filepath = file.path;
+      var filename = path.basename(filepath, '.js');
+
+      global.sourceFiles[filename] =
+        filepath.replace('tests', 'src').replace('.js', '');
+    }))
     .pipe(mocha());
 });
 
